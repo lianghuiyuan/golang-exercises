14b539618962d3956924c13abd3880fa66759930
Liang Huiyuan <lianghuiyuan@jetio.com.cn>
2024-11-22 16:47:57 +0800

    vault backup: 2024-11-22 16:47:57

--- practices/main.go
+++ practices/main.go
@@ -5,25 +5,26 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "从 %v 获取数据失败: %v\n", url, err)
-			continue //继续下一个循环
-			// os.Exit(1)  //直接退出程序
+			continue
 		}
-		fmt.Println("11111111111111111: %s", url)
-		b, err := io.ReadAll(resp.Body)   
+		fmt.Printf("状态码: %s\n", resp.Status)
+		body, err := io.ReadAll(resp.Body)
 		resp.Body.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "解析 body 数据失败: %v\n", err)
-			continue // 继续下一个循环
-			// os.Exit(1) // 直接退出程序
+			fmt.Fprintf(os.Stderr, "读取数据失败: %v\n", err)
+			continue
 		}
-		fmt.Println(b)
-		// fmt.Printf("%s", b)
+		fmt.Printf("%s", body)
 	}
 }
