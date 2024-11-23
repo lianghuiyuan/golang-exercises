df04cf2a073bb41efcd90bd20063112c408b54b5
Liang Huiyuan <lianghuiyuan@jetio.com.cn>
2024-11-24 00:06:45 +0800

    vault backup: 2024-11-24 00:06:45

--- practices/main.go
+++ practices/main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http://") {
-			url = "http://" + url
-		}
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "从 %v 获取数据失败: %v\n", url, err)
-			continue
-		}
-		fmt.Printf("状态码: %s\n", resp.Status)
-		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "读取数据失败: %v\n", err)
-			continue
-		}
-		fmt.Printf("%s", body)
-	}
+	var p = new([]int)
+	fmt.Println("p=", p)
+	fmt.Println("*p=", *p)
+	//*p = 1
+	//fmt.Println("p=", p)
+	//fmt.Println("*p=", *p)
+
+	var q = make([]int, 5, 10)
+	fmt.Println("q=", q)
 }
